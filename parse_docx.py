4a6fe7102a1d2ccfcdc4f0e2a9ccb14ecfb8b404
SeriousAlexej <seriousalexej@gmail.com>
2017-10-02 14:34:47 +0300

    faster, stronger, better regexes!

--- parse_docx.py
+++ parse_docx.py
@@ -1,11 +1,27 @@
 import nltk.tokenize
 import re
 
-listHead = re.compile(r"^[ \t]*\(?([a-zA-Z]|\d+)(\)|\.)?[ \t]+\w")
+listEnumerator = r"([a-zA-Z]|\d+|•)"
+
+listTypesArray =\
+    [
+        r"\(?" +listEnumerator + r"\)",
+        listEnumerator + r"\.?"
+    ]
+
+listTypesReg = ""
+for listType in listTypesArray:
+    if listTypesReg != "":
+        listTypesReg += "|"
+    listTypesReg += "(" + listType + ")"
+listStart = r"^[ \t]*"
+listEnd = r"[ \t]+\w"
+listRegex = re.compile(listStart + "(" + listTypesReg + ")" + listEnd)
+
 
 with open("text.txt") as file:
     raw = file.read()
     tokens = nltk.sent_tokenize(raw)
     for token in tokens:
-        if re.search(listHead, token) != None:
+        if re.search(listRegex, token) != None:
             print(token)
