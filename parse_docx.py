baafab4e96a74567eaf1b621c70a1b3e4be7804d
kpashko <pashkokostya@gmail.com>
2017-10-02 11:56:45 +0300

    Initial commit

---
+++ parse_docx.py
@@ -0,0 +1,9 @@
+import nltk.tokenize
+
+with open("text.txt") as file:
+    raw = file.read()
+    tokens = nltk.sent_tokenize(raw)
+    print(tokens)
+    for t in tokens:
+        if t[0]>='0' and t[0]<='9':
+            print(t)
